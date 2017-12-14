de548a78a628b698956727c6e1b8488a1648f73f
up730418 <up730418@port.ac.uk>
2017-12-14 14:06:00 +0000

    comments

--- js/hueComands.js
+++ js/hueComands.js
@@ -5,11 +5,14 @@ let bridge;
 let username;
 let hueData = {};
 
+//Find a bridge and conncect to hues
+// Only works if bride button has been pressed in last 30 seconds
 function startSearching(){
   findHue();
   connectBridge();
 }
 
+// change the brightness, hue, saturation and on/off of all lights
 function changeLights(bri, hue, sat, on) {
   user.getLights().then((res) => {
     for(i in res) {
@@ -20,6 +23,7 @@ function changeLights(bri, hue, sat, on) {
   })
 }
 
+// Change the hue of all lights
 function changeLightsHue(hue) {
   user.getLights().then((res) => {
     for(i in res) {
@@ -28,31 +32,36 @@ function changeLightsHue(hue) {
     }
   })
 }
+// Chnage a specific lights (num) brightness, hue, saturation, on/off
 function changeLight(num, bri, hue, sat, on) {
 
   user.setLightState(num, {"on": on,"bri": bri,"sat": sat,"hue": hue})
 }
+
+//Change all lights to a random colour
 function party(){
   let max = 65280;
   let min = 0;
   let rand = Math.floor(Math.random() * (max - min) + min);
-  console.log(rand);
+
   changeLights(255, rand, 255, true);
 }
 
+// Change all lights to a diffrent random colour
 function party2() {
   for(let i=1; i <= 3; i++) {
     let max = 65280;
     let min = 0;
     let rand = Math.floor(Math.random() * (max - min) + min);
-    console.log(rand);
+ 
     changeLight(i, 255, rand, 255, true);
   }
 }
 
+// Connect to a bridge
 function connectBridge(){
   bridge = hue.bridge(ip);
-  console.log(bridge);
+
   // create user account (requires link button to be pressed)
   bridge.createUser('myApp#testdevice').then(data => {
 
@@ -64,8 +73,6 @@ function connectBridge(){
         // extract bridge-generated username from returned data
         username = data[0].success.username;
 
-        console.log('New username:', username);
-
         // instantiate user object with username
         user = bridge.user(username);
         hueData = {};
@@ -74,9 +81,6 @@ function connectBridge(){
         hueData.username = username;
 
         localStorage.hueData = JSON.stringify(hueData);
-        //user.setLightState(1, { on: true }).then(data => {
-        // process response data, do other things
-        //});
 
         showFunctions();
 
@@ -84,14 +88,14 @@ function connectBridge(){
   });
 }
 
+ // If already conected before re connect with same user
 function findHue(){
-  // If already conected before re connect and change lights as conformation
+ 
   if(localStorage.hueData){
     console.log("connecting from saved user");
     hueData = localStorage.hueData;
     bridge = hue.bridge(hueData.ip);
     user = bridge.user(hueData.username);
-    //party();
   }
 
   hue.discover().then(bridges => {
@@ -99,9 +103,7 @@ function findHue(){
         console.log('No bridges found. :(');
     }
     else {
-        bridges.forEach(b =>  {ip = b.internalipaddress
-                                console.log(ip)
-                              });
+        bridges.forEach(b =>  {ip = b.internalipaddress});
     }
   }).catch(e => console.log('Error finding bridges', e));
 }
